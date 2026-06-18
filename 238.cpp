834d94627136749ab542c10b28042f1230c29329
Varun28Maurya <varun28maurya@gmail.com>
2026-06-18 10:50:13 +0530

    238

--- 238.cpp
+++ 238.cpp
@@ -4,14 +4,28 @@ int main()
 {
     int nums[4]={1,2,3,4};
     int n=sizeof(nums)/sizeof(int);
-    int ans=nums[0];
-    for(int i=1;i<n;i++)
+    int ans[n];
+    /*for(int i=1;i<n;i++)
     {
         ans*=nums[i];
     }
     for(int i=0;i<n;i++)
     {
         cout<<ans/nums[i]<<",";
+    }
+        */
+    for(int i=0;i<n;i++)
+    {
+        int prod=1;
+        for(int j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                prod*=nums[j];
+            }
+        }
+        ans[i]=prod;
+       cout<<ans[i]<<",";
     }
     return 0;
 }
