03c438857859063dbccfc595333b5dd8cdc24f5d
Varun28Maurya <varun28maurya@gmail.com>
2026-06-18 11:26:00 +0530

    Space

--- 238.cpp
+++ 238.cpp
@@ -5,7 +5,10 @@ int main()
     int nums[4]={1,2,3,4};
     int n=sizeof(nums)/sizeof(int);
     int ans[n];
-    /*for(int i=1;i<n;i++)
+    /*
+    O(n) but the constraints and division is used in this 
+    
+    for(int i=1;i<n;i++)
     {
         ans*=nums[i];
     }
@@ -14,6 +17,9 @@ int main()
         cout<<ans/nums[i]<<",";
     }
         */
+    /*
+    O(n^2)
+
     for(int i=0;i<n;i++)
     {
         int prod=1;
@@ -26,6 +32,25 @@ int main()
         }
         ans[i]=prod;
        cout<<ans[i]<<",";
+    }
+       */
+    //prefix
+    int prefix[n];
+    prefix[0]=1;
+    for(int i=1;i<n;i++)
+    {
+        prefix[i]=prefix[i-1]*nums[i-1];
+    }
+    int suffix[n];
+    suffix[n-1]=1;
+    for(int i=n-2;i>=0;i--){
+        suffix[i]=suffix[i+1]*nums[i+1];
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        ans[i]=prefix[i]*suffix[i];
+        cout<<ans[i]<<endl;
     }
     return 0;
 }
@@ -48,6 +73,62 @@ public:
                     ans[i]*=nums[j];
                 }
             }
+        }
+        return ans;
+    }
+};
+
+*/
+
+/*
+Here the time complexity is good but the space complexity is worst
+O(n)
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n,1);
+        vector<int> prefix(n,1);
+        vector<int> suffix(n,1);
+
+        for(int i=1;i<n;i++)
+        {
+            prefix[i]=prefix[i-1]*nums[i-1];
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            suffix[i]=suffix[i+1]*nums[i+1];
+        }
+        for(int i=0;i<n;i++)
+        {
+            ans[i]=prefix[i]*suffix[i];
+        }
+        return ans;
+    }
+};
+
+*/
+
+/*
+Time : O(n)
+Space : O(1)
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n,1);
+        int prefix=1,suffix=1;
+
+        for(int i=1;i<n;i++)
+        {
+            ans[i]=ans[i-1]*nums[i-1];
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            suffix*=nums[i+1];
+            ans[i]*=suffix;
         }
         return ans;
     }
